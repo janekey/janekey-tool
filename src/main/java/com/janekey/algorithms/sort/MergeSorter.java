800c24d793b5a810a56854eb7c9aa471293dbb9c
janekey <zheng.q31@gmail.com>
2013-05-16 18:14:08 +0800

    sort algorithm 归并

--- src/main/java/com/janekey/algorithms/sort/MergeSorter.java
+++ src/main/java/com/janekey/algorithms/sort/MergeSorter.java
@@ -1,15 +1,49 @@
 package com.janekey.algorithms.sort;
 
 /**
+ * 归并排序，将两个已经排好序的序列合并成一个序列的操作。
+ * 最差时间复杂度O(nlogn) 最优时间复杂度O(n) 平均时间复杂度O(nlogn)
+ * 最差空间复杂度O(n)
  * User: qi.zheng
  * Date: 13-5-14
  * Time: 下午5:59
  */
 public class MergeSorter<E extends Comparable<E>> extends BaseSorter<E> {
     @Override
+    @SuppressWarnings("unchecked")
     public void sort(E[] array) {
-
+        Object[] tmp = new Comparable[array.length];
+        mergeSort(array, 0, array.length - 1, (E[]) tmp);
     }
 
+    private void mergeSort(E[] array, int left, int right, E[] tmp) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(array, left, mid, tmp);
+            mergeSort(array, mid + 1, right, tmp);
+            merge(array, tmp, left, mid + 1, right);
+        }
+    }
 
+    private void merge(E[] array, E[] tmp, int lPos, int rPos, int rEnd) {
+        int lEnd = rPos - 1;
+        int tPos = lPos;    //tmp position
+        int lPosInit = lPos;
+
+        while (lPos <= lEnd && rPos <= rEnd) {
+            if (array[lPos].compareTo(array[rPos]) <= 0)
+                tmp[tPos++] = array[lPos++];
+            else
+                tmp[tPos++] = array[rPos++];
+        }
+
+        while (lPos <= lEnd)
+            tmp[tPos++] = array[lPos++];
+        while (rPos <= rEnd)
+            tmp[tPos++] = array[rPos++];
+
+        // 将排好序的临时数组中放回源数组
+        for (; rEnd >= lPosInit; rEnd--)
+            array[rEnd] = tmp[rEnd];
+    }
 }
--- src/main/java/com/janekey/algorithms/sort/Tester.java
+++ src/main/java/com/janekey/algorithms/sort/Tester.java
@@ -8,25 +8,25 @@ public class Tester {
 	
 	public static void main(String[] args) {
 		
-		test(new InsertSorter<Integer>());// 插入排序
-		test(new BubbleSorter<Integer>());// 冒泡排序
-		test(new SelectSorter<Integer>());// 选择排序
+//		test(new InsertSorter<Integer>());// 插入排序
+//		test(new BubbleSorter<Integer>());// 冒泡排序
+//		test(new SelectSorter<Integer>());// 选择排序
 		test(new QuickSorter<Integer>());// 快速排序
-		test(new ShellSorter<Integer>());// 希尔排序
+//		test(new ShellSorter<Integer>());// 希尔排序
+		test(new MergeSorter<Integer>());// 归并排序
 
 //		displayArray(list);
 
 	}
 	
 	public static void test(BaseSorter<Integer> sorter) {
-		int size = 1000;
+		int size = 2000000;
 		Integer[] list = new Integer[size];
         Random random = new Random();
 		for(int i = 0; i < size; i++) {
-			list[i] = random.nextInt(500);
+			list[i] = random.nextInt(2000000);
 		}
 		
-//		System.out.println("size : " + list.length);
 		long startTime = Calendar.getInstance().getTimeInMillis();
 
 //        System.out.println(Arrays.toString(list));
