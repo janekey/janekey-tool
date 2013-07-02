9a98854a1ccc27dba3543f552a121f74bbec7b77
janekey <zheng.q31@gmail.com>
2013-07-02 19:22:16 +0800

    new algorithm

--- src/main/java/com/janekey/algorithms/test/ListTester.java
+++ src/main/java/com/janekey/algorithms/test/ListTester.java
@@ -41,6 +41,34 @@ public class ListTester {
         }
     }
 
+    public static Entry mergeSorted(Entry a, Entry b) {
+        if (a == null) return b;
+        if (b == null) return a;
+        Entry newHead = new Entry(0, a);
+        Entry tmp = newHead;
+        while (a != null && b != null) {
+            if (a.value < b.value) {
+                tmp.next = a;
+                a = a.next;
+            } else {
+                tmp.next = b;
+                b = b.next;
+            }
+            tmp = tmp.next;
+        }
+        while (a != null) {
+            tmp.next = a;
+            tmp = tmp.next;
+            a = a.next;
+        }
+        while (b != null) {
+            tmp.next = b;
+            tmp = tmp.next;
+            b = b.next;
+        }
+        return newHead.next;
+    }
+
     static class Entry {
         int value;
         Entry next;
@@ -51,8 +79,13 @@ public class ListTester {
         }
     }
 
+    static Random r = new Random();
     public static void main(String[] args) {
-        Random r = new Random();
+//        testReverse();
+        testMerge();
+    }
+
+    public static void testReverse() {
         Entry pre = null;
         for (int i = 0; i < 10; i++) {
             pre = new Entry(r.nextInt(10), pre);
@@ -70,6 +103,18 @@ public class ListTester {
                 tmp = tmp.next;
             }
         }
+    }
+
+    public static void testMerge() {
+        Entry a = null, b = null;
+        for (int i = 0; i < 10; i++) {
+            a = new Entry(10 * (9 - i) + r.nextInt(10), a);
+            b = new Entry(10 * (9 - i) + r.nextInt(10), b);
+        }
+        displayList(a);
+        displayList(b);
+        Entry c = mergeSorted(a, b);
+        displayList(c);
     }
 
     static void displayList(Entry head) {
---
+++ src/main/java/com/janekey/algorithms/test/StringTester.java
@@ -0,0 +1,63 @@
+package com.janekey.algorithms.test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * User: qi.zheng
+ * Date: 13-7-2
+ * Time: 下午4:28
+ */
+public class StringTester {
+    static HashSet<String> list = new HashSet<String>();
+    static void permutation(char[] str, int i) {
+        if (i >= str.length) return;
+
+        if (i == (str.length - 1)) {
+            list.add(String.valueOf(str));
+        } else {
+            for (int j = i; j < str.length; j++) {
+                char tmp = str[i];
+                str[i] = str[j];
+                str[j] = tmp;
+
+                permutation(str, i + 1);
+
+                tmp = str[i];
+                str[i] = str[j];
+                str[j] = tmp;
+            }
+        }
+    }
+
+    static String reverse(String str) {
+        if (str == null || str.length() == 0)
+            return "";
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length / 2; i++) {
+            char tmp = chars[i];
+            chars[i] = chars[chars.length - i - 1];
+            chars[chars.length - i - 1] = tmp;
+        }
+        return String.valueOf(chars);
+    }
+
+    static void testPermutation() {
+        String a = "sssd";
+        permutation(a.toCharArray(), 0);
+        System.out.println(list.size());
+        System.out.println(Arrays.toString(list.toArray()));
+    }
+
+    static void testStrReverse() {
+        String a = "asldkjfslkj";
+        System.out.println(a);
+        System.out.println(reverse(a));
+    }
+
+    public static void main(String[] args) {
+//        testPermutation();
+        testStrReverse();
+    }
+
+}
